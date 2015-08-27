ba67ae5a5f0b237c61c7b8c54203921e1d12a4a3
Eugene Murray <eugene.murray@gmail.com>
2015-08-27 16:13:20 +0000

    first commit - add files

---
+++ README.md
@@ -0,0 +1,1 @@
+https://em-cloud-d3dashboard-eugene-murray.c9.io/dist/index.html
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('d3dashboard')
+  .controller('MainController', function ($scope) {
+        this.userName = 'Eugene Murray';
+        this.helloText = 'Welcome to D3 Dashboard';
+        this.descriptionText = 'AngularJS, D3';
+    });
